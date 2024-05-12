ed1f436d5bd7f852bb5b3e83f1399b1d64c6c10b
malzahnparker <malzahnparker@gmail.com>
2024-05-12 22:19:34 +0000

    publish github

---
+++ content_scripts.js
@@ -0,0 +1,13 @@
+(() => {
+    let youtubeLeftControls, youtubePlayer;
+    let currentVideo = "";
+    let currentVideoBookmarks = [];
+
+    chrome.runtime.onMessage.addListener((obj, sender, response) => {
+        const { type, value, videoId } = obj;
+
+        if (type === "NEW") {
+            currentVideo = videoId;
+            newSearch();
+        }
+    });
